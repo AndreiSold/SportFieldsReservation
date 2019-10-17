fd47d2b9d8bf93baa580260a83d1a0c5e909e454
Andrei Sold <sold_andrei@yahoo.com>
2019-10-17 09:03:11 +0300

    Firebase added to the project

--- app/src/main/java/andrei/sold/upt/com/sportfieldsreservation/RegisterActivity.java
+++ app/src/main/java/andrei/sold/upt/com/sportfieldsreservation/RegisterActivity.java
@@ -1,15 +1,24 @@
 package andrei.sold.upt.com.sportfieldsreservation;
 
 import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private DatabaseReference userRef;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
 
     }
 }
