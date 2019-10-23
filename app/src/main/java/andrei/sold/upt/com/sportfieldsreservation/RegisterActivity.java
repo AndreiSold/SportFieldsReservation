69d220428cbec9864e09577141259cd5e0f7c217
Andrei Sold <sold_andrei@yahoo.com>
2019-10-23 23:03:50 +0300

    Changed age to birthdate in RegisterUser

--- app/src/main/java/andrei/sold/upt/com/sportfieldsreservation/RegisterActivity.java
+++ app/src/main/java/andrei/sold/upt/com/sportfieldsreservation/RegisterActivity.java
@@ -1,16 +1,22 @@
 package andrei.sold.upt.com.sportfieldsreservation;
 
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import andrei.sold.upt.com.sportfieldsreservation.Models.User;
 import androidx.annotation.Nullable;
@@ -19,21 +25,24 @@ import androidx.appcompat.app.AppCompatActivity;
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     private DatabaseReference userDatabaseRef;
-    private EditText nameUser, ageUser, locationUser, numberUser;
+    private EditText nameUser, birthdateUser, locationUser, numberUser;
     private MaterialButton nextButton;
     private ProgressDialog progressDialog;
 
     private String id;
+    private Calendar myCalendar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        myCalendar = Calendar.getInstance();
+
         nextButton = (MaterialButton) findViewById(R.id.nextButtonRegister);
 
         nameUser = (EditText) findViewById(R.id.nameUser);
-        ageUser = (EditText) findViewById(R.id.ageUser);
+        birthdateUser = (EditText) findViewById(R.id.birthdateUser);
         locationUser = (EditText) findViewById(R.id.locationUser);
         numberUser = (EditText) findViewById(R.id.numberUser);
 
@@ -42,6 +51,32 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         nextButton.setOnClickListener(this);
 
         progressDialog = new ProgressDialog(this);
+
+        final DatePickerDialog.OnDateSetListener date = initializeDatePicker();
+        birthdateUser.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(RegisterActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private DatePickerDialog.OnDateSetListener initializeDatePicker() {
+        return new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
     }
 
     @Override
@@ -55,7 +90,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private void registerUser() {
 
         String name = nameUser.getText().toString().trim();
-        String age = ageUser.getText().toString().trim();
+        String age = birthdateUser.getText().toString().trim();
         String location = locationUser.getText().toString().trim();
         String number = numberUser.getText().toString().trim();
 
@@ -83,5 +118,12 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             return true;
         }
         return false;
+    }
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        birthdateUser.setText(sdf.format(myCalendar.getTime()));
     }
 }
