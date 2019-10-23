535eff3539355587358975fa896c22ff1246725f
Andrei Sold <sold_andrei@yahoo.com>
2019-10-23 21:49:59 +0300

    Added layout for RegisterCredentialsActivity

--- app/src/main/java/andrei/sold/upt/com/sportfieldsreservation/RegisterCredentialsActivity.java
+++ app/src/main/java/andrei/sold/upt/com/sportfieldsreservation/RegisterCredentialsActivity.java
@@ -1,6 +1,21 @@
 package andrei.sold.upt.com.sportfieldsreservation;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.database.FirebaseDatabase;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterCredentialsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credentials);
+
+    }
 }
